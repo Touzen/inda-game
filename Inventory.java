2de2a38e6fa0f3291f40a2644259945c27e9d0a3
Thomas Vakili <thomas@vakili.net>
2013-12-10 17:20:53 +0100

    Added Inventory class. Not done yet, this is just a draft.

---
+++ Inventory.java
@@ -0,0 +1,38 @@
+/**
+ * A class to hold a characters inventory.
+ *
+ * @author Thomas Vakili
+ * @version 2013.12.10
+*/
+public class Inventory {
+    private ArrayList<Item> items;
+    private int size;
+
+    /**
+     * Adds an item to the inventory.
+     *
+     * @param item The Item to add
+     * @return true if there is room, false if the inventory is full
+    */
+    public boolean addItem(Item item) {
+        if (items.size() < size) {
+            items.add(item);
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Gets the number of items in the inventory
+     *
+     * @return The number of items
+    */
+    public int numberOfItems() {
+        return items.size();
+    }
+
+    public void setSize(int newSize) {
+        size = newSize;
+    }
+
+}
